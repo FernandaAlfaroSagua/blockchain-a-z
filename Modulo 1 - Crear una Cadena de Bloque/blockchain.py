b9ceb82d9cbd13148d083c8d97d0c96113db830a
FernandaAlfaroSagua <80983740+FernandaAlfaroSagua@users.noreply.github.com>
2022-04-04 23:02:29 -0400

    Importar Librerias

--- Modulo 1 - Crear una Cadena de Bloque/blockchain.py
+++ Modulo 1 - Crear una Cadena de Bloque/blockchain.py
@@ -6,3 +6,18 @@ Created on Mon Apr  4 11:56:25 2022
 """
 
 # Modulo 1 - Crear una Cadena de Bloque
+
+# Para Instalar:
+# Flask==1.1.2: pip install Flask==1.1.2
+# Cliente HTTP Postman: https://www.getpostman.com/
+
+# Importar las librerias
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+# Parte 1 - Crear la Cadena de Bloques
+
+
+# Parte 2 - Minado de un Bloque de la Cadena
